3ddd2aa2fb7544c89d7c37ff2d631e6d42cbd8ab
Terry Hayes <terrychayes@gmail.com>
2016-07-17 17:47:33 -0400

    Updated jury.rb to pass tests through test_cast_votes_hash_includes_two_finalists. Printing each vote to terminal doesn't work yet.

--- lib/jury.rb
+++ lib/jury.rb
@@ -3,6 +3,7 @@ class Jury
 
 	def initialize
 		@members = []
+		@finalists = {}
 	end
 
 	def add_member(member)
@@ -11,5 +12,24 @@ class Jury
 
 	def members
 		@members
+	end
+
+	def cast_votes(finalists)
+		# The first line below is for the initial "rigged" tests where the finalists aren't random.
+		#finalists.each {|finalist| @finalists[finalist] = 0}
+		vote_count = Hash.new(0)
+		#@members.each {|member| vote_count[member]}
+		@members.each do |member|
+			vote = @members.sample
+			vote_count[vote] += 1
+			#This part below is somehow breaking my code.
+			#puts "#{member} voted for #{@members[vote]}."
+		end
+		vote_count.sort_by {|k, v| v}
+		2.times do
+			key, value = vote_count.shift
+			@finalists[key] = value
+		end
+		return @finalists
 	end
 end
