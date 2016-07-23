493218d92f7299280efa13c1e0e5da42ca88a7c6
Terry Hayes <terrychayes@gmail.com>
2016-07-22 20:13:10 -0400

    Completed tribe.rb; passing all tests.

--- lib/tribe.rb
+++ lib/tribe.rb
@@ -1,9 +1,22 @@
 class Tribe
-	attr_reader :name, :members
+	attr_accessor :name, :members
 
-	def initialize(tribe)
+	def initialize(tribe) #Input should be hash of name: "string" and members: @contestants array
 		@name = tribe[:name]
 		@members = tribe[:members]
-		puts self.name #This seems wierd, but using temporarily to pass test.
+		puts self.name
+	end
+
+	def to_s
+		@name
+	end
+
+	def tribal_council(immune)
+		safe = immune[:immune]
+		@members.shuffle #Initial shuffle to keep voted off contestant random.
+		while @members.last == safe #Keep shuffling until last member is not immune.
+			@members.shuffle
+		end
+		@members.pop #Remove last member from @members
 	end
 end
